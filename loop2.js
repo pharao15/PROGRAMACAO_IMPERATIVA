a9f08ff0435e8694a8670ccf8d0813888689e037
pharao15 <fgrahal@uol.com.br>
2021-06-20 12:30:23 -0300

    microdesafio 2 de loops adicionado

---
+++ loop2.js
@@ -0,0 +1,19 @@
+console.log('Criada uma tabela inicial com nome de flores');
+console.log(' ');
+flores = ['rosa','cravo', 'gerânio', 'orquidea','crisântemo', 'violeta', 'margarida', 'girassol'];
+function percorre() 
+         {
+          for (i=0 ; i<flores.length ; i++)
+              {
+                   console.log('Elemento ' + (i+1) + " na posição " + i + " = " + flores[i])
+              }
+         }
+percorre();
+console.log(' ');
+console.log ('Agora vamos alterar e mostrar...');
+console.log(' ');
+console.log ('colocando duas flores diferentes, uma no começo, outra no fim');
+flores.unshift('azaleia');
+flores.push('begonia');
+console.log(' ');
+percorre();
