57b7dc9c3a627235c98937f240bef91a799411e9
pharao15 <fgrahal@uol.com.br>
2021-06-20 12:10:23 -0300

    microdesafio percorre array adicionado

---
+++ percorrearray.js
@@ -0,0 +1,6 @@
+console.log('Imprime um a um os elementos da variavel unidimensional pontos')
+const pontos = [12,34,56,34,5,8,76,4,1,90];
+for (i=0 ; i<pontos.length ; i++)
+    {
+        console.log('Elemento ' + (i+1) + " na posição " + i + " = " + pontos[i])
+    }
