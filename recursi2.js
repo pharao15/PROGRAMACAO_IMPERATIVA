fb29e7cc8deaca568dc4ea4029cc905041f15152
pharao15 <fgrahal@uol.com.br>
2021-06-22 07:03:08 -0300

    Microdesafio Java Avançado fatorial

---
+++ recursi2.js
@@ -0,0 +1,12 @@
+
+
+function fatorial(num)
+{
+    resulta=1;
+    for(i=1 ; i<=num ; i++){ resulta*=i}
+    console.log('Fatorial de ' + num + ' = ' + resulta);
+}
+fatorial(1);
+fatorial(2);
+fatorial(3);
+fatorial(12);
