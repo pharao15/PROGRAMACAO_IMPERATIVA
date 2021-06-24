7b1fa20fd2496d8fe5a8ad50a85faf224aaab034
pharao15 <fgrahal@uol.com.br>
2021-06-24 17:18:39 -0300

    Microdesafio Objetos Literais

---
+++ restaurante.js
@@ -0,0 +1,14 @@
+let Restaurante = {
+    nome : 'Belavista',
+    cardapioDegusta :['Salada Niçoise','Steak au Poivre Vert', ' 1 Taça Vinho Tinto Petrus 1982',
+    'Creme Brulèe','Investimento = R$ 3.000,00'],
+};
+function entrada(){
+    console.log('Bem Vindo ao Restaurante ' + Restaurante.nome + '!' );
+    console.log("Na Pandemia, só trabalhamos com nosso Menu Degustação Abaixo:");
+    console.log(Restaurante.cardapioDegusta);
+    console.log("Aproveite sua experiênicia! Vive la France!!!!")
+}
+
+//console.log(Restaurante.nome);
+entrada();
