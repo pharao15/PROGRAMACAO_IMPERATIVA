40d3b399530ff33d7346744b8861862693b333f9
pharao15 <fgrahal@uol.com.br>
2021-06-25 07:34:03 -0300

    Microdesafio 2 Spread...está certo isso?

--- spread2.js
+++ spread2.js
@@ -1,3 +1,16 @@
 let numerosPrimos = [3,7,11,13,17,23,31];
 let numeros = [...numerosPrimos,32,33,34,35,36, ...numerosPrimos];
 console.log(numeros);
+
+//numeros = [];
+numeros = [23,24,12,43,5,87,56];
+//vou imprimir numeros e depois chamar a função maiorNumero que devolve 
+//o menor valor;
+console.log(...numeros);
+function maiorNumero(numeros){
+    
+    return console.log(Math.min(...numeros));}
+
+maiorNumero(numeros);
+
+
