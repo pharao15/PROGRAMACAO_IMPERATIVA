a0b375b7151e1975ea5a15e979906a6ed56e7ec3
pharao15 <fgrahal@uol.com.br>
2021-07-02 09:03:52 -0300

    algumas alterações realizadas

--- compras.js
+++ compras.js
@@ -2,13 +2,13 @@ let listaCompra = ['rucula', 'agrião', 'rabanete', 'tomilho', 'salsão', 'manje
 console.log('Esta é a listinha do carnívoro disfarçado de coelho...');
 console.log(listaCompra)
 console.log('Se voce não gostou dessa forma de apresentar a lista vou juntar tudo com uma barra')
-console.log('Pra fazer isso usamos o comando join(), compras.join()')
+console.log('Pra fazer isso usamos o comando join(), compras.join("/")')
 let barra = listaCompra.join('/')
 console.log(barra)
 console.log('Já o comando compras.pop() elimina o ultimo elemento e devolve o valor removido...')
 let remove = listaCompra.pop()
 console.log(listaCompra)
-console.log(remove)
+console.log('Removi ', remove, '.')
 console.log('Comando compras.push() adiciona um ou mais elementos ao final do array e retorna um numero de elementos')
 console.log('Se essa lista já tava grande, agora extravasou de vez')
 let elemento = listaCompra.length
