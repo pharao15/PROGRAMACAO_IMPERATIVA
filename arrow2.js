25b7409ad40d95044c303f7671f668d95dcafd66
pharao15 <fgrahal@uol.com.br>
2021-07-02 10:30:45 -0300

    alterações no codigo Microdesafio Arrow

--- arrow2.js
+++ arrow2.js
@@ -2,3 +2,9 @@ let print=(mensagem)=>console.log(mensagem);
 let soma=(n1,n2)=>  n1+n2;
 print('ola mundo');
 console.log(soma(5,10));
+
+// outra forma de escrever o codigo acima para o print que só tem 1 argumento
+let print2 = mensagem=> console.log (mensagem);
+let soma2=(n1,n2)=>  n1+n2;
+print2('ola mundo');
+console.log(soma2(8,13));
