41e9f1a50c2b4cf36c382117afed28d971fe374f
pharao15 <fgrahal@uol.com.br>
2021-07-02 11:10:56 -0300

    exemplo do PG, modificação_2

--- callback3.js
+++ callback3.js
@@ -3,9 +3,15 @@ function fullName(nome,sobrenome)
         return nome + ' ' + sobrenome;
     }
 
+function iniciais(nome,sobrenome)
+    {
+        return nome[0]  + sobrenome[0];
+    }
+
 function bomDia(nome,sobrenome,callback)
     {
         return 'Bom dia, ' + callback(nome,sobrenome);
     }
 
 console.log(bomDia('Fabio','Gomide',fullName));
+console.log(bomDia('Fabio','Gomide',iniciais))
