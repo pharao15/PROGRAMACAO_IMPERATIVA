68c7234e8d336140d08042e34896256f9523e23e
pharao15 <fgrahal@uol.com.br>
2021-07-04 12:12:37 -0300

    codigo refeito com função preparo

--- microondas.js
+++ microondas.js
@@ -1,80 +1,67 @@
-function menu(prato, tempoPreparo) {
-    switch (prato) {
-        case 1:
-            if (tempoPreparo >= 30) console.log('KABUUM')
+let prato=['Pipoca','Macarrão','Carne','Feijão','Brigadeiro'];
+let tempoPrato=[10,8,15,12,8];
 
-            else if (tempoPreparo >= 20) console.log('Pipoca queimou')
+function preparo(i, tempoEscolhido)
+    {   
+       // console.log("indicie=",i);
+       // console.log('Tempo Escolhido= ',tempoEscolhido);
+       // console.log(prato[i],tempoPrato[i]);
 
-            else if (tempoPreparo < 10) console.log('tempo insuficiente')
+          if (tempoEscolhido>=tempoPrato[i]*3) {console.log('KABUUM');}
 
-            else if(tempoPreparo == undefined) console.log('tempo indefinindo')
+          else if (tempoEscolhido >= tempoPrato[i]*2) {console.log(prato[i] + ' queimou.');}
 
-            else console.log('Pipoca pronto, Bom apetite')
+          else if (tempoEscolhido < tempoPrato[i])
+                  {
+                    console.log('tempo insuficiente pra ' + prato[i]);
+                  }
 
-            break
+          else if(tempoEscolhido == undefined) {console.log('tempo indefinindo');}
 
-            case 2:
-            if (tempoPreparo >= 24) console.log('KABUUM')
+          else if(tempoEscolhido==tempoPrato[i]) {console.log(prato[i] + ' pronto, Bom apetite')}
 
-            else if (tempoPreparo >= 16) console.log('macarrão queimou')
-
-            else if (tempoPreparo < 8) console.log('tempo insuficiente')
-
-            else if(tempoPreparo == undefined) console.log('tempo indefinindo')
-
-            else console.log('macarrão pronto, Bom apetite')
-
-            break
-
-            case 3:
-            if (tempoPreparo >= 45) console.log('KABUUM')
-
-            else if (tempoPreparo >= 30) console.log('Carne queimou')
-
-            else if (tempoPreparo < 15) console.log('tempo insuficiente')
-
-            else if(tempoPreparo == undefined) console.log('tempo indefinindo')
-
-            else console.log('Carne pronto, Bom apetite')
-
-            break
-
-            case 4:
-            if (tempoPreparo >= 36) console.log('KABUUM')
-
-            else if (tempoPreparo >= 24) console.log('feijão queimou')
-
-            else if (tempoPreparo < 12) console.log('tempo insuficiente')
-
-            else if(tempoPreparo == undefined) console.log('tempo indefinindo')
-
-            else console.log('feijão pronto, Bom apetite')
-
-            break
-
-            case 5:
-            if (tempoPreparo >= 24) console.log('KABUUM')
-
-            else if (tempoPreparo >= 16) console.log('brigadeiro queimou')
-
-            else if (tempoPreparo < 8) console.log('tempo insuficiente')
-
-            else if(tempoPreparo == undefined) console.log('tempo indefinindo')
-
-            else console.log('brigadeiro pronto, Bom apetite')
-
-            break
+          
+    }
             
-            default: console.log('prato Inexistente')
+function menu(escolhePrato, escolheTempo) {
+    i=escolhePrato-1;
+
+    switch (escolhePrato) {
+        
+        case 1:
+            preparo(i,escolheTempo);
+        break;
+
+        case 2:
+            preparo(i,escolheTempo);
+        break;
+
+        case 3:
+            preparo(i,escolheTempo);
+        break;
+
+        case 4:
+            preparo(i,escolheTempo)
+        break;
+
+        case 5:
+            preparo(i,escolheTempo);
+        break;    
+            
+        default: console.log('prato Inexistente')
+        break;
+
     }
 }
 
 
-menu(1, )
-menu(2, 20)
-menu(3, 15)
-menu(4,25)
-menu(5,30)
-menu(6,10)
-menu(3,20)
-menu(4,15)
+
+menu(1,);
+menu(null,24);
+menu();
+//menu(3,45);
+menu(4,36);
+menu(5,24);
+menu(3,14);
+menu(4,12);
+
