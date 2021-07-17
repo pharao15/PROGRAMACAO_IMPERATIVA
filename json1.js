1e2b9dfe586334b2b12a852d4eb5c501cef2d591
pharao15 <fgrahal@uol.com.br>
2021-07-17 11:02:44 -0300

    Microdesafios 1 e 2 JSON

---
+++ json1.js
@@ -0,0 +1,12 @@
+// devolvendo em JavaScript
+
+let dadosJson = '{"cidade":"São Paulo","bairro":"Morumbi"}';
+let dadosConvert = JSON.parse(dadosJson);
+console.table(dadosConvert);
+
+
+// devolvendo em JSON
+
+let objLit = { nome:'Carla',pais:'Brasil', vero:true, ddi:55, estadoSul: ['RS','SC','PR'], misc: {a:'portugues',b:'latino',c:'BR'}};
+let dadosConv = JSON.stringify(objLit);
+console.log(dadosConv);
