bc3eeb407ff7b2d08dfbdbb72e349b482f4f33b4
pharao15 <fgrahal@uol.com.br>
2021-07-17 11:30:00 -0300

    Microdesafio 2 JSON alterado

--- json1.js
+++ json1.js
@@ -10,3 +10,5 @@ console.table(dadosConvert);
 let objLit = { nome:'Carla',pais:'Brasil', vero:true, ddi:55, estadoSul: ['RS','SC','PR'], misc: {a:'portugues',b:'latino',c:'BR'}};
 let dadosConv = JSON.stringify(objLit);
 console.log(dadosConv);
+console.log(objLit);
+console.log(objLit.nome," ",objLit.vero, "  ", objLit.estadoSul[0]);
